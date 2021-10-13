691386f8bcdfc1d8706da9c5e47b0552d1306519
DiegoCedilloCastillo <dcedillocas@gmail.com>
2021-10-12 23:09:53 -0500

    Agregar archivos de proyecto.

---
+++ appCsHerencia/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace appHerencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Punto2D a;
+            a = new Punto2D(-1.45, -2.88);
+            Console.WriteLine(a.ToString());
+
+            Punto2D b;
+            b = new Punto2D(3.55, 9.02);
+            Console.WriteLine(b.ToString()); */
+
+            Punto3D a;
+            a = new Punto3D(2, 2, 4);
+            Console.WriteLine(a.ToString());
+
+            Punto3D b;
+            b = new Punto3D(3, 3, 7);
+            Console.WriteLine(b.ToString());
+
+            double d = a.Distancia(b);
+            Console.WriteLine("Distancia " + d);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ appCsHerencia/Punto3D.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appHerencia
+{
+    class Punto3D : Punto2D
+    {
+        protected double z;
+
+        public double Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+
+        public Punto3D() : base()
+        {
+            z = 0;
+        }
+        public Punto3D(double x, double y, double z) : base(x, y)
+        {
+            this.z = z;
+        }
+        public double Distancia(Punto3D p)
+        {
+            double d = 0;
+            double dx = x - p.x;
+            double dy = y - p.y;
+            double dz = z - p.z;
+            /*double cdx = Math.Pow(dx, 2);
+            double cdy = Math.Pow(dy, 2);
+            double cdz = Math.Pow(dz, 2); */
+            //d = Math.Sqrt(cdx + cdy + cdz);
+            d = Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2) + Math.Pow(dz, 2));
+
+            return d;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " Z=" + z;
+        }
+    }
+}
